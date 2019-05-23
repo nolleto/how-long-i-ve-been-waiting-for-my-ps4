c979a8d4a9155a3c6ffb009af505ddcff7ebc98f
nolleto <felipenolletonascimento@gmail.com>
2019-05-23 17:03:13 -0300

    Fixed word conjugation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ const Footer = () => (
     <div className='footer__line'/>
 
     <p>
-      Made with <span className='footer--red'>❤︎</span> <span className='footer--angry'>and with a little of angry</span> by <a href='https://twitter.com/FelipeNolleto'>nolleto</a>.
+      Made with <span className='footer--red'>❤︎</span> <span className='footer--angry'>and with a little of anger</span> by <a href='https://twitter.com/FelipeNolleto'>nolleto</a>.
       Find this on <a href='https://github.com/nolleto/how-long-i-ve-been-waiting-for-my-ps4'>github</a></p>
   </footer>
 )
